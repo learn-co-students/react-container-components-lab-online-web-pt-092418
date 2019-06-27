59589bf74e276aa969c8e34da859b0f03889da65
github username <github email address>
2019-06-27 17:16:15 -0400

    reset the map syntax

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,20 +1,23 @@
 
-import React, { Component } from 'react';
+import React from 'react'
+
+const Review = ({display_title, summary_short}) => {
+  return (
+    <div className="review" key={display_title}>
+      <h4>{display_title}</h4>
+      <p>{summary_short}</p>
+    </div>
+  )
+}
 
 
+const MovieReviews = ({reviews}) => {
+  return (
+    <div className="review-list">
+      {reviews.map(Review)}
+    </div>
+  )
+}
 
-const MovieReviews = ({reviews}) => (
-      <div className="review-list">
-        {reviews.map(review => <Review /> )}
-      </div>
-
-);
 
 export default MovieReviews;
-
-const Review = ({display_title, summary_short}) => (
-  <div className="review">
-    <h4>{display_title}</h4>
-    <p>{summary_short}</p>
-  </div>
-)
