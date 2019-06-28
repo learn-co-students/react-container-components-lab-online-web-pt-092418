bd648d6694799500fa1700e25324187859414698
github username <github email address>
2019-06-28 11:07:16 -0400

    wip - add form in searchableReviewsContainer

--- src/components/SearchableMovieReviewsContainer.js
+++ src/components/SearchableMovieReviewsContainer.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import 'isomorphic-fetch';
-// import MovieReviews from './MovieReviews'
+import MovieReviews from './MovieReviews'
 
 const NYT_API_KEY = 'f98593a095b44546bf4073744b540da0';
 const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/all.json?'
@@ -18,12 +18,25 @@ class SearchableMovieReviewsContainer extends Component {
   render() {
     return (
       <div className="searchable-movie-reviews">
+        <form>
+          <input type="text"></input>
+          <input type="submit"></input>
+
+        </form>
         <MovieReviews reviews={this.state.reviews} />
       </div>
     );
   }
 
-  // <MovieReviews />
+  componentDidMount() {
+    fetch(`https://api.nytimes.com/svc/movies/v2/reviews/search.json?query=${this.state.searchTerm}?api-key=ZAKLHDCpHwELxnf94wxbpQ3lLWev8wcm`)
+    .then(res => res.json)
+    .then(json => {
+      setState:({
+        reviews: json.results
+      })
+    })
+  }
 }
 
 export default SearchableMovieReviewsContainer;
